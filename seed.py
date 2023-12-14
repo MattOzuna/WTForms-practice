8210a849e16501980393e6d709f761e31b8e43d1
Matt Ozuna <m.ozuna22@gmail.com>
2023-12-14 15:19:56 -0500

    added second pet to seed file

--- seed.py
+++ seed.py
@@ -11,6 +11,13 @@ with app.app_context():
                   photo_url='https://i.etsystatic.com/6226037/r/il/be2698/2006003775/il_1588xN.2006003775_kwvx.jpg',
                   notes='My first Dog', 
                   available=False)
+    
+    new_pet2 = Pet(name='Madison',
+                  species='Dog', 
+                  age=7, 
+                  photo_url='https://www.rvc.ac.uk/Media/Default/VetCompass/Chocolate%20Lab%20on%20Grass.jpg',
+                  notes='My Second Dog', 
+                  available=False)
 
-    db.session.add(new_pet)
+    db.session.add_all([new_pet, new_pet2])
     db.session.commit()
